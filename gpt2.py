1104080eb5bb962d8d2c8647899a0c7f5942aa0a
Xiongfei Wei <isaacwxf23@gmail.com>
2025-03-23 23:07:13 +0000

    create a duplicate of gpt2.py and added skeleton comment
    Signed-off-by: Xiongfei Wei <isaacwxf23@gmail.com>

---
+++ gpt2.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+# hyperparameters
+
+
+# seed
+
+
+# prepare the input
+
+# write encoder and decoder
+
+
+# Train and test splits
+
+
+# data loading
+
+
+# write estimate_loss
+
+
+#######################################
+# model code begins
+# Head, MultiHeadAttention, FeedForward, Block, GPTLanguageModel
+
+
+
+#######################################
+# model code ends
+
+
+# initialize the model
+
+
+# create a PyTorch optimizer
+
+
+# kick off the training.
+
+
+# generate from the model.
